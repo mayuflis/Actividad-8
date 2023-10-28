851a17e580773e22dbe2ca2ecaa99b46f6e9035b
SergioDev <mayitavista@gmail.com>
2023-10-28 19:39:21 +0200

    configuración de rutas

---
+++ src/controller/inmueble.controller.js
@@ -0,0 +1,9 @@
+const getAllInmuebles = async (req, res) => {};
+
+const getInmuebleById = async (req, res) => {};
+
+const createInmueble = async (req, res) => {};
+
+const updateInmueble = async (req, res) => {};
+
+const deleteInmueble = (async = (req, res) => {});
